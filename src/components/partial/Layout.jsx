6d851abb25af52de5aa161f48ee535fe45fa4d00
khatereh <khaterehnaseri444@gmail.com>
2025-08-31 11:12:58 +0330

    feat(layout) : hide header and sidebar from login page

--- src/components/partial/Layout.jsx
+++ src/components/partial/Layout.jsx
@@ -1,17 +1,25 @@
+import { useLocation } from "react-router-dom";
 import Header from "../ui/organisms/Header/Header";
 import SideBar from "../ui/organisms/SideBar/SideBar";
 
 function Layout({ children }) {
+  const location = useLocation();
   return (
-    <div className="flex h-screen">
-      <SideBar /> 
-      <div className="flex-1 flex flex-col">
-        <Header /> 
-        <main className="flex-1 overflow-auto bg-[#F4F4F5]">
-          {children} 
-        </main>
-      </div>
-    </div>
+    <>
+      {location.pathname === '/login' ? (<>
+        {children}
+      </>) : (
+        <div className="flex h-screen">
+          <SideBar />
+          <div className="flex-1 flex flex-col">
+            <Header />
+            <main className="flex-1 overflow-auto bg-[#F4F4F5]">
+              {children}
+            </main>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
