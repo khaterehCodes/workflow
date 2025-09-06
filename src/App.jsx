1d5449d4cb034a59f017eefe10b4d55d05c60891
khatereh <khaterehnaseri444@gmail.com>
2025-09-06 12:44:18 +0330

    refactor:fix bugs in layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/partial/Layout';
 import LoginPage from './components/ui/pages/login/LoginPage';
-<<<<<<< HEAD
 import UserListPage from './components/ui/pages/UserListPage/UserListPage';
 import EditUsersPage from './components/ui/pages/EditUsers/EditUsers';
-=======
 import SignupPage from './components/ui/pages/signupPage/SignupPage';
 import ResetPasswordPage from './components/ui/pages/resetPasswordPage/ResetPasswordPage';
 import ResetEmailPage from './components/ui/pages/resetEmailPage/ResetEmailPage';
 import ConfirmEmail from './components/ui/pages/confirmEmail/ConfirmEmail';
->>>>>>> a29d3200b7293bb2a9da747085ce1a21209a4abf
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path='/login' element={<LoginPage/>}/>
-<<<<<<< HEAD
-          <Route index element={<UserListPage/>}/>
-          <Route path='/edituser' element={<EditUsersPage/>}/>
-=======
-          <Route path='/signup' element={<SignupPage/>}/>
-          <Route path='/resetPassword' element={<ResetPasswordPage/>}/>
-          <Route path='/resetEmail' element={<ResetEmailPage/>}/>
-          <Route path='/confirmEmail' element={<ConfirmEmail/>}/>
->>>>>>> a29d3200b7293bb2a9da747085ce1a21209a4abf
+          <Route path='/login' element={<LoginPage />} />
+          <Route index element={<UserListPage />} />
+          <Route path='/edituser' element={<EditUsersPage />} />
+          <Route path='/signup' element={<SignupPage />} />
+          <Route path='/resetPassword' element={<ResetPasswordPage />} />
+          <Route path='/resetEmail' element={<ResetEmailPage />} />
+          <Route path='/confirmEmail' element={<ConfirmEmail />} />
         </Routes>
       </Layout>
     </BrowserRouter>
